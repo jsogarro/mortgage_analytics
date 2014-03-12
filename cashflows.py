2429a19c1a4f199fe055e71788b4bd5a5c4a3691
Jamal O'Garro <jamal.ogarro@gmail.com>
2014-03-11 20:07:54 -0400

    more code clean up

--- cashflows.py
+++ cashflows.py
@@ -59,20 +59,20 @@ def rmbs_show(self):
     def cal_rmbs():
         self.period = int(term.get())
         self.mortgage_cf = []
-        #cb_mortgage_type.select()
+        # cb_mortgage_type.select()
         # calculate the SMM based on assumed CPR
         smm = 1 - math.pow(( 1 - float(self.rmbs_cpr.get())),1/12.0)
-        #print smm
-        #print "SMM %f" % smm
+        # print smm
+        # print "SMM %f" % smm
         balance = float(e.get())
-        #print balance
+        # print balance
         period = int(term.get())
-        #mortgage_rate = 
+        # mortgage_rate = 
  
         r = float(mortgage_rate.get())/12.0
-        #print self.rmbs_mortgageType.get()
+        # print self.rmbs_mortgageType.get()
         if self.rmbs_mortgageType.get() == 1 :
-        #fix principal method
+        # fix principal method
             monthly_principal = balance / float(period)
             for i in range(period):
                 mcf = 0.0
@@ -90,7 +90,7 @@ def rmbs_show(self):
                 self.mortgage_cf.append(mcf)
         #fix amount method
         else:
-            mthpyt = balance * r * math.pow((1+r),period)/ (math.pow((1+r),period) -1)
+            mthpyt = balance * r * math.pow((1+r),period) / (math.pow((1 + r),period) -1)
             for i in range(period):
                 mcf = 0.0
                 interest = balance * r
@@ -108,7 +108,7 @@ def rmbs_show(self):
  
                 self.mortgage_cf.append(mcf)
  
-        #self.mortgage_cf = x for x in self.mortgage_cf if x > 0
+        # self.mortgage_cf = x for x in self.mortgage_cf if x > 0
  
         self.senior_cf = []
         self.junior_cf = []
@@ -116,15 +116,14 @@ def rmbs_show(self):
         senior_req = float(de.get()) * float(de_rate.get())
         junior_req = float(de1.get()) * float(de_rate1.get())
 
-        ## this is the waterfall
-        ## individual cash flows or total cashflows for a period?
+        # this is the waterfall
         for i in self.mortgage_cf:
             if i <= senior_req:
                 # if we have less than the senior requirement then only the senior tranche is paid
                 self.senior_cf.append(i)
                 self.junior_cf.append(0)
                 self.equity_cf.append(0)
-            elif i <=  senior_req+junior_req:
+            elif i <=  senior_req + junior_req:
                 # append the full senior requirement to the tranche
                 # and give the remainder to the senior, equity tranche gets nothing
                 self.senior_cf.append(senior_req)      
@@ -134,28 +133,22 @@ def rmbs_show(self):
                 # if monthly cashflows are sufficient then all tranches get paid 
                 self.senior_cf.append(senior_req)
                 self.junior_cf.append(junior_req)
-                self.equity_cf.append(i-senior_req-junior_req )
+                self.equity_cf.append(i - senior_req - junior_req)
  
         self.rmbs_render()
  
  
 def rmbs_render(self):
-    #print self.mortgage_cf
-    #print self.senior_cf
-    #print self.junior_cf
-    #print self.equity_cf
- 
     plt.cla()
     x = np.arange(0.0, len(self.mortgage_cf), 1)
  
     from pylab import *
  
-    plot(x, self.mortgage_cf,color='#ff9999',linewidth=2.0, label='Total MBS Cash Flow')
-    plot(x, self.equity_cf,color='#BB1BF5',linewidth=2.0, label='Equity Tranche Cash Flow')
-    plot(x, self.junior_cf,color='#2340E8',linewidth=2.0, label='Junior Tranche Cash Flow')
-    plot(x, self.senior_cf,color='#1DF0A2',linewidth=2.0, label='Senior Tranche Cash Flow')
+    plot(x, self.mortgage_cf,color = '#ff9999',linewidth = 2.0, label = 'Total MBS Cash Flow')
+    plot(x, self.equity_cf,color = '#BB1BF5',linewidth = 2.0, label = 'Equity Tranche Cash Flow')
+    plot(x, self.junior_cf,color = '#2340E8',linewidth = 2.0, label = 'Junior Tranche Cash Flow')
+    plot(x, self.senior_cf,color = '#1DF0A2',linewidth = 2.0, label = 'Senior Tranche Cash Flow')
  
     legend( ('Total MBS Cash Flow', 'Equity Tranche Cash Flow', 'Junior Tranche Cash Flow','Senior Tranche Cash Flow'), loc='upper right')
-    #plt.fill_between(x, self.mortgage_cf,0, 'r')
     plt.grid(True)
     plt.show()
